c37c1a329406c367ad25009b25e3e065cdc6f45c
Jesse Boettcher <jesse.boettcher@gmail.com>
2022-03-22 07:13:32 -0700

    Placeholder endpoints for games

--- webserver/api/game.py
+++ webserver/api/game.py
@@ -13,3 +13,27 @@ from webserver.logging import write_log
 '''
 APIs for managing updates to games
 '''
+
+@app.route('/api/games/<team_id>', methods=['GET'])
+def get_games(team_id):
+    '''
+    Returns all games for the specified team. Includes flag for whether the game was completed.
+    '''
+    return make_response({'result': 'success'})
+
+
+@app.route('/api/game/<game_id>', methods=['GET'])
+def get_game(team_id):
+    '''
+    Returns the details for the specified game: game date/time, rink, home/away, vs, player replies
+    '''
+    return make_response({'result': 'success'})
+
+
+@app.route('/api/game/reply', methods=['GET', 'POST'])
+def game_reply(team_id):
+    '''
+    POST with game_id, response (yes|no|maybe), message
+    GET for replies from email links with url params containing the same fields
+    '''
+    return make_response({'result': 'success'})
